709804524d4aca1f536d99e6ccd9cb669714d8b9
jakecernet <117542696+jakecernet@users.noreply.github.com>
2023-05-19 16:16:18 +0200

    fixed some issues|

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
 // Retrieve existing notes from local storage
 let notes = JSON.parse(localStorage.getItem('notes')) || [];
-let filteredNotes = []; // Array to store filtered notes
 
 // Display existing notes
 function displayNotes() {
     const notesContainer = document.getElementById('notes');
     notesContainer.innerHTML = '';
 
-    if (filteredNotes.length === 0) {
-        notesContainer.innerHTML = '<p>No notes found</p>';
+    if (notes.length === 0) {
+        notesContainer.innerHTML = '<p>No notes yet</p>';
     } else {
-        filteredNotes.forEach(function (note, index) {
+        notes.forEach(function (note, index) {
             const noteElement = document.createElement('div');
             noteElement.className = 'note';
             noteElement.innerHTML = `
                 <span class="note-number">${index + 1}.</span>
-                <span>${note.title}</span>
-                <button class="edit-btn" onclick="editNote(${index})">Edit</button>
+                <span>${note}</span>
+                <button class="edit-btn" onclick="editNote(${index})">✎</button>
                 <button class="delete-btn" onclick="deleteNote(${index})">Delete</button>
             `;
             notesContainer.appendChild(noteElement);
@@ -30,11 +29,10 @@ function addNote() {
     const noteText = noteInput.value.trim();
 
     if (noteText !== '') {
-        const newNote = { title: noteText, content: '', tags: [], date: new Date().toISOString() };
-        notes.push(newNote);
+        notes.push(noteText);
         localStorage.setItem('notes', JSON.stringify(notes));
         noteInput.value = '';
-        filterNotes(document.getElementById('search-input').value.trim()); // Reapply filtering
+        displayNotes();
     }
 }
 
@@ -42,39 +40,86 @@ function addNote() {
 function deleteNote(index) {
     notes.splice(index, 1);
     localStorage.setItem('notes', JSON.stringify(notes));
-    filterNotes(document.getElementById('search-input').value.trim()); // Reapply filtering
+    displayNotes();
 }
 
 // Edit a note
+// ...
+
+// Edit a note
 function editNote(index) {
-    // Edit note functionality
-}
+    const noteElement = document.getElementById('notes').children[index];
+    const noteText = noteElement.querySelector('span:not(.note-number)').textContent;
 
-// Sort notes based on the selected criterion
-function sortNotes(criterion) {
-    filteredNotes.sort(function (a, b) {
-        if (criterion === 'title') {
-            return a.title.localeCompare(b.title);
-        } else if (criterion === 'date') {
-            return new Date(a.date) - new Date(b.date);
+    // Replace the note text with an input field
+    const inputElement = document.createElement('input');
+    inputElement.type = 'text';
+    inputElement.value = noteText;
+    noteElement.replaceChild(inputElement, noteElement.querySelector('span:not(.note-number)'));
+
+    // Replace the Edit button with Save and Cancel buttons
+    const editBtn = noteElement.querySelector('.edit-btn');
+    editBtn.textContent = 'Save';
+    editBtn.onclick = function () {
+        saveNoteEdit(index);
+    };
+
+    const cancelBtn = document.createElement('button');
+    cancelBtn.className = 'cancel-btn';
+    cancelBtn.textContent = 'Cancel';
+    cancelBtn.onclick = function () {
+        cancelNoteEdit(index, noteText);
+    };
+    noteElement.appendChild(cancelBtn);
+
+    // Focus on the input field
+    inputElement.focus();
+
+    // Add event listeners for Enter and Escape keys
+    inputElement.addEventListener('keyup', function (event) {
+        if (event.keyCode === 13) {
+            saveNoteEdit(index);
+        } else if (event.keyCode === 27) {
+            cancelNoteEdit(index, noteText);
         }
-        return 0;
     });
-
-    displayNotes();
 }
 
-// Filter notes based on the keyword or tag
-function filterNotes(keyword) {
-    filteredNotes = notes.filter(function (note) {
-        return note.title.toLowerCase().includes(keyword.toLowerCase()) ||
-            note.tags.some(function (tag) {
-                return tag.toLowerCase().includes(keyword.toLowerCase());
-            });
-    });
+// Save edited note
+function saveNoteEdit(index) {
+    const noteElement = document.getElementById('notes').children[index];
+    const inputElement = noteElement.querySelector('input');
+    const newNoteText = inputElement.value.trim();
 
-    displayNotes();
+    if (newNoteText !== '') {
+        notes[index] = newNoteText;
+        localStorage.setItem('notes', JSON.stringify(notes));
+        displayNotes();
+    }
 }
+
+// Cancel editing a note
+function cancelNoteEdit(index, oldNoteText) {
+    const noteElement = document.getElementById('notes').children[index];
+    const inputElement = noteElement.querySelector('input');
+
+    inputElement.value = oldNoteText;
+    noteElement.replaceChild(document.createElement('span'), inputElement);
+    noteElement.querySelector('.edit-btn').textContent = 'Edit';
+    noteElement.removeChild(noteElement.querySelector('.cancel-btn'));
+}
+
+//listen for escape key, close edit mode
+document.addEventListener('keydown', function (event) {
+    if (event.keyCode === 27) {
+        const editBtn = document.querySelector('.edit-btn');
+        if (editBtn) {
+            const index = editBtn.parentNode.querySelector('.note-number').textContent - 1;
+            const noteText = notes[index];
+            cancelNoteEdit(index, noteText);
+        }
+    }
+});
 
 // Event listeners
 document.getElementById('add-btn').addEventListener('click', addNote);
@@ -84,10 +129,5 @@ document.getElementById('note-input').addEventListener('keyup', function (event)
     }
 });
 
-document.getElementById('search-input').addEventListener('input', function (event) {
-    const keyword = event.target.value.trim();
-    filterNotes(keyword);
-});
-
 // Initial display of notes
-filterNotes(''); // Display all notes initially
+displayNotes();
